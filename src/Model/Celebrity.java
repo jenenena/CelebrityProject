a9fb82604a8d3fbf4cb30233ab811a24014797b1
Jenna Mills <superangelfish555@gmail.com>
2019-03-21 09:14:10 -0600

    Work on  Celebrity
    Added a couple data members to Celebrity, added framework of methods to controller

--- src/Model/Celebrity.java
+++ src/Model/Celebrity.java
@@ -3,4 +3,44 @@ package Model;
 public class Celebrity
 {
 
+	private String name;
+	private String gender;
+	private int age;
+	private String profession;
+	
+	private String clue;
+	
+	//-----------------//
+	
+	public Celebrity(String name, String clue)
+	{
+		this.name = name;
+		this.clue = clue;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getGender()
+	{
+		return gender;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	public String getProfession()
+	{
+		return profession;
+	}
+	
+	public String getClue()
+	{
+		return clue;
+	}
+	
 }
--- src/controller/Runner.java
+++ src/controller/Runner.java
@@ -3,4 +3,9 @@ package controller;
 public class Runner
 {
 
+	public static void main(String [] args)
+	{
+		CelebrityController app = new CelebrityController;
+		app.start();
+	}
 }
