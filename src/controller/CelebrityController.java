098332e3015d4dff01694be2f941f5df528abd83
Jenna Mills <superangelfish555@gmail.com>
2019-03-21 09:49:00 -0600

    Worked on Controller
    Completed a lot of method stubs!

--- src/controller/CelebrityController.java
+++ src/controller/CelebrityController.java
@@ -1,34 +1,78 @@
 package controller;
 
+import model.Celebrity;
+import java.util.ArrayList;
+
 public class CelebrityController
 {
+	
+	private ArrayList <Celebrity> celebrityList;
+	private String currentClue;
+	private Celebrity currentCelebrity;
+	
+	public CelebrityController()
+	{
+		this.celebrityList = new ArrayList<Celebrity>();
+	}
 
-	public void start()
+	
+	private void start()
 	{
 		
 	}
-	
-	public CelebrityController()
+
+	private Celebrity currentCelebrity()
 	{
 		
 	}
 	
 	public String getClue()
 	{
-		
-	}
-	
-	public boolean checkClue(String)
-	{
-		
+		return currentCelebrity.getClue();
 	}
 	
 	public Celebrity getRandomCelebrity()
 	{
+		int randomIndex = (int)(Math.random() * celebrityList.size());
+		currentCelebrity = celebrityList.remove(randomIndex);
+		return currentCelebrity;
+	}	
+	
+	public String checkGuess(String guess)
+	{
+		String response = "";
+		if(guess.equalsIgnoreCase(currentCelebrity.getName()))
+		{
+			response = "You got it!";
+			if(celebrityList.size() != 0)
+			{
+				response += getRandomCelebrity().getClue();
+			}
+			else
+			{
+				response += "Game over... You WIN!";
+			}
+		}
+		else
+		{
+			response = "Sorry, that's not right. Try again." + currentCelebrity.getClue();
+		}
+		return response;
+	}
+	
+	public boolean checkClue(String clue)
+	{
 		
 	}
+	
+	public void addCelebrity(String name, String clue, String type)
+	{
+		Celebrity toBeAdded = new Celebrity(name, clue);
+		celebrityList.add(toBeAdded);
+	}
 
-	public void addCelebrity(String name, String gender, int age, String profession)
+	
+	public void playGame()
 	{
 		
 	}
